38e7f3ec4ac8f96accdd5e4df21d2b58fb90da30
adil <itsadil7227@gmail.com>
2025-10-21 22:19:32 +0530

    footer change

--- src/mobizone/Footer.js
+++ src/mobizone/Footer.js
@@ -33,9 +33,9 @@ const Footer = () => {
                     </div>
                 </section>
                 <h4 className='text-white text-center border-primary mt-5'>&copy;Design & Developed By Adil Khan</h4>
-                <h5 className='text-danger text-center p-1'>All Rights Reservrd By MOBI ZONE&reg;</h5>
-                <p className='text-white text-center pt-2 m-0'>📍 Hitech City, Madhapur, Hyderabad – 500081</p>
-                <p className='text-white text-center '>📞 +91 1111111111 | ✉️ info@example.com</p>
+                <h5 className='text-white text-center p-1'>All Rights Reservrd By MOBI ZONE&reg;</h5>
+                <p className='text-info text-center pt-2 m-0'>📍 Hitech City, Madhapur, Hyderabad – 500081</p>
+                <p className='text-info text-center '>📞 +91 1111111111 | ✉️ info@example.com</p>
             </section>
         </div>
     )
